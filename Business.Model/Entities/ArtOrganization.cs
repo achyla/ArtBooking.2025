59ae13ef7c1548e7471b5accc714177c2de27628
achyla <148362165+achyla@users.noreply.github.com>
2025-03-27 21:23:32 +0100

    Update modeli, dodano walidacje

--- Business.Model/Entities/ArtOrganization.cs
+++ Business.Model/Entities/ArtOrganization.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Business.Model.Entities;
 
 public class ArtOrganization
 {
     public int ArtOrganizationId { get; set; }
+    
+    [Required, StringLength(100, MinimumLength = 1)]
     public string Name { get; set; }
+
+    [Required, StringLength(500, MinimumLength = 1)]
     public string Description { get; set; }
+
+    [Required, EmailAddress, StringLength(254)]
     public string Email { get; set; }
 
     // Address
--- Business.Model/Entities/User.cs
+++ Business.Model/Entities/User.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Business.Model.Enums;
 
 namespace Business.Model.Entities;
@@ -5,11 +6,22 @@ namespace Business.Model.Entities;
 public class User
 {
     public int UserId { get; set; }
+    
+    [Required, StringLength(50, MinimumLength = 3)]
     public string LoginName { get; set; }
+
+    [Required] // zakładam, że hash ma maksymalną długość
     public string PasswordHash { get; set; }
+
+    [Required, EmailAddress, StringLength(254)]
     public string Email { get; set; }
+
+    [Required, StringLength(50, MinimumLength = 1)]
     public string FirstName { get; set; }
+
+    [Required, StringLength(50, MinimumLength = 1)]
     public string LastName { get; set; }
+    
     public UserRole? Role { get; set; }
     public int? ArtOrganizationId { get; set; }
     public virtual ArtOrganization? ArtOrganization { get; set; }
