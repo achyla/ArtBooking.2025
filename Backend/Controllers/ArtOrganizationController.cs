46d03b84e07abf998606eae25b0c9b92108f73da
achyla <148362165+achyla@users.noreply.github.com>
2025-03-28 01:00:25 +0100

    ArtOrgnizationController development - Get all organizations, Edit (HttpPut), Remove (HttpDelete)

--- Backend/Controllers/ArtOrganizationController.cs
+++ Backend/Controllers/ArtOrganizationController.cs
@@ -1,6 +1,8 @@
 using Business.Model.Data;
 using Business.Model.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -16,6 +18,10 @@ public class ArtOrganizationController : ControllerBase
     [HttpPost]
     public ActionResult<ArtOrganization> CreateOrganization(ArtOrganization organization)
     {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
         try
         {
             _dbContext.Add(organization);
@@ -31,10 +37,29 @@ public class ArtOrganizationController : ControllerBase
             );
         }
 
-        return CreatedAtAction(nameof(CreateOrganization), new { organization.ArtOrganizationId }, organization);
+        return CreatedAtAction(nameof(GetOrganization), new { id = organization.ArtOrganizationId }, organization);
     }
 
     [HttpGet]
+    public ActionResult<IEnumerable<ArtOrganization>> GetAllOrganizations()
+    {
+        try{
+            var organizations = _dbContext.ArtOrganizations.ToList();
+            if(organizations == null) return NoContent();
+            return Ok(organizations);
+        }
+        catch (Exception exp)
+        {
+            return Problem(
+                statusCode: 500,
+                title: "An unexpected error occured",
+                // just for debugging purposes
+                detail: exp.Message
+            );
+        }
+    }
+
+    [HttpGet("{id}")]
     public ActionResult<ArtOrganization> GetOrganization(int id)
     {
         try
@@ -58,4 +83,73 @@ public class ArtOrganizationController : ControllerBase
             );
         }
     }
+
+
+
+
+    [HttpPut("{id}")]
+    public ActionResult<ArtOrganization> EditOrganization(int id, ArtOrganization organization)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        try
+        {      
+            var organizationOld = _dbContext.ArtOrganizations.Find(id);
+
+            if (organizationOld == null) return Problem(
+                statusCode: 404,
+                title: "Organization cannot be found",
+                detail: $"Organization with id:{id} cannot be found!"
+            );
+            
+            organizationOld.Name = organization.Name;
+            organizationOld.Description = organization.Description;
+            organizationOld.Email = organization.Email;
+
+            _dbContext.SaveChanges();
+            return Ok(organizationOld);
+
+        }
+        catch (Exception exp)
+        {
+            return Problem(
+                statusCode: 500,
+                title: "An unexpected error occured",
+                // just for debugging purposes
+                detail: exp.Message
+            );
+        }
+    }
+    
+    [HttpDelete("{id}")]
+    public ActionResult<ArtOrganization> DeleteOrganization(int id)
+    {
+        try
+        {
+            var organization = _dbContext.ArtOrganizations.Find(id);
+            if (organization == null) return Problem(
+                    statusCode: 404,
+                    title: "Organization cannot be found",
+                    detail: $"Organization with id:{id} cannot be found!"
+            );
+
+            _dbContext.Remove(organization);
+            _dbContext.SaveChanges();
+
+            return Ok(organization);
+        }
+        catch (Exception exp)
+        {
+            return Problem(
+                statusCode: 500,
+                title: "An unexpected error occured",
+                // just for debugging purposes
+                detail: exp.Message
+            );
+        }
+
+    }
+    
 }
